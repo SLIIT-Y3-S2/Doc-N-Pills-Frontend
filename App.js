ebb05bd22b6804ee47e4307d991d821d5b555275
CHATHUMI <it20385064@my.sliit.lk>
2022-11-04 20:23:46 +0530

    login UIs created

--- App.js
+++ App.js
@@ -1,12 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import 'react-native-gesture-handler';
+import Login from './components/Login';
+import Register from './components/Register';
 
 export default function App() {
+  const Stack = createStackNavigator();
   return (
-    <View style={styles.container}>
-      <Text>Doc And Pills!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Login" component={Login} />
+          <Stack.Screen name="Register" component={Register} />
+          {/* <Stack.Screen
+            name="Easy Going"
+            component={BottomNavBar}
+            options={{ headerShown: false }}
+          /> */}
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
 
