d3608f615d5199c49c34d29bebb946e9584ee770
Dishancp <it20012106@my.sliit.lk>
2022-11-05 23:18:52 +0530

    Medicine forms interfaces created

---
+++ components/AddMedicineForm.js
@@ -0,0 +1,137 @@
+import React from "react";
+import { SafeAreaView, Text, ScrollView, StyleSheet, View } from "react-native";
+import { TextInput, Button } from "react-native-paper";
+import { useState } from "react";
+
+const AddMedicineForm = () => {
+  const [validated, setvalidated] = useState(false);
+  const [bname, setBrandName] = useState("");
+  const [mterm, setMedicalTerm] = useState("");
+  const [type, setType] = useState("");
+  const [stock, setStock] = useState("");
+  const [price, setPrice] = useState("");
+  const [dose, setDose] = useState("");
+
+  const Resetform = () => {
+    setBrandName(null);
+    setMedicalTerm(null);
+    setType(null);
+    setStock(null);
+    setPrice(null);
+    setDose(null);
+  };
+
+  return (
+    <ScrollView style={styles.view}>
+      <SafeAreaView style={styles.form}>
+        <TextInput
+          label="Brand Name"
+          placeholder="Enter Brand Name"
+          value={bname}
+          style={styles.input}
+          onChangeText={(text) => setBrandName(text)}
+          mode="outlined"
+          outlineColor="black"
+          activeOutlineColor="#1e90ff"
+        />
+
+        <TextInput
+          label="Medical Term"
+          placeholder="Enter Medical Term"
+          value={mterm}
+          style={styles.input}
+          onChangeText={(text) => setMedicalTerm(text)}
+          mode="outlined"
+          outlineColor="black"
+          activeOutlineColor="#1e90ff"
+        />
+
+        <TextInput
+          label="Type"
+          placeholder="Enter Type"
+          value={type}
+          style={styles.input}
+          onChangeText={(text) => setType(text)}
+          mode="outlined"
+          outlineColor="black"
+          activeOutlineColor="#1e90ff"
+        />
+
+        <TextInput
+          label="Stock"
+          placeholder="Enter Stock"
+          value={stock}
+          style={styles.input}
+          keyboardType="numeric"
+          onChangeText={(text) => setStock(text)}
+          mode="outlined"
+          outlineColor="black"
+          activeOutlineColor="#1e90ff"
+        />
+
+        <TextInput
+          label="Price"
+          placeholder="LKR 0.00"
+          value={price}
+          style={styles.input}
+          keyboardType="numeric"
+          onChangeText={(text) => setPrice(text)}
+          mode="outlined"
+          outlineColor="black"
+          activeOutlineColor="#1e90ff"
+        />
+
+        <TextInput
+          label="Dose"
+          placeholder="Enter Dose"
+          value={dose}
+          style={styles.input}
+          onChangeText={(text) => setDose(text)}
+          mode="outlined"
+          outlineColor="black"
+          activeOutlineColor="#1e90ff"
+        />
+
+        <Button
+          onPress={() => console.log(bname)}
+          mode="contained"
+          buttonColor="#1e90ff"
+          style={styles.button}
+        >
+          ADD
+        </Button>
+
+        <Button
+          onPress={() => Resetform()}
+          mode="contained"
+          buttonColor="#87cefa"
+          style={styles.button}
+        >
+          RESET
+        </Button>
+      </SafeAreaView>
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  input: {
+    margin: 12,
+  },
+  button: {
+    margin: 12,
+  },
+  label: {
+    fontWeight: "bold",
+    padding: 5,
+  },
+  view: {
+    backgroundColor: "white",
+    padding: 10,
+  },
+  form: {
+    padding: 5,
+  },
+});
+
+export default AddMedicineForm;
