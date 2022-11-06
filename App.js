427b36e5c2aa218772c2f218d58eb62ca7f38ac5
RisinaR <rkarisina@gmail.com>
2022-11-06 16:22:44 +0530

    Create participant register form

--- App.js
+++ App.js
@@ -1,23 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import 'react-native-gesture-handler';
-import React from 'react';
-import PharmacyBottomNavBar from './components/PharmacyBottomNavBar';
-import AddMedicineForm from './components/AddMedicineForm'
-import UpdateMedicineForm from './components/UpdateMedicineForm'
+import { StatusBar } from "expo-status-bar";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import "react-native-gesture-handler";
+import React from "react";
+import PharmacyBottomNavBar from "./components/PharmacyBottomNavBar";
+import AddMedicineForm from "./components/AddMedicineForm";
+import UpdateMedicineForm from "./components/UpdateMedicineForm";
+import PatientBottomNavBar from "./components/PatientBottomNavBar";
+import PatientRegisterForm from "./components/PatientRegisterForm";
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="DocNPills" component={PharmacyBottomNavBar} options={{ headerShown: false }} /> 
-          <Stack.Screen name="Add Medicine" component={AddMedicineForm} />
-          <Stack.Screen name="Update Medicine" component={UpdateMedicineForm} />
-        </Stack.Navigator>
+      <Stack.Navigator>
+        {/* <Stack.Screen
+          name="DocNPills"
+          component={PharmacyBottomNavBar}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen name="Add Medicine" component={AddMedicineForm} />
+        <Stack.Screen name="Update Medicine" component={UpdateMedicineForm} /> */}
+        <Stack.Screen
+          name="PatientNavBar"
+          component={PatientBottomNavBar}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen name="Register" component={PatientRegisterForm} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
- 
