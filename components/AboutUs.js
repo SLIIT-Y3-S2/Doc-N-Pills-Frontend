b3441b9b12ef733fab17e423f017658c3217ee1f
Dishancp <it20012106@my.sliit.lk>
2022-11-07 12:22:39 +0530

    About us and pharmacy profile views created

--- components/AboutUs.js
+++ components/AboutUs.js
@@ -2,6 +2,7 @@ import React from "react";
 import { View, Image } from "react-native";
 import { Avatar, Button, Card, Title, Paragraph } from "react-native-paper";
 import Ionicons from "react-native-vector-icons/Ionicons";
+import { Linking} from 'react-native'
 
 const AboutUs = () => {
   return (
@@ -20,41 +21,44 @@ const AboutUs = () => {
         </Card.Content>
       </Card>
 
-      <Card>
-        <Card.Content>
-          <Paragraph>
-            Facebook{"     "}
-            <Ionicons name="logo-facebook" size={20} color="blue" />
-          </Paragraph>
-        </Card.Content>
-      </Card>
+      <View style={{flexDirection:'row',justifyContent:'space-evenly', marginTop:20}}>
+        <Ionicons 
+          name="logo-facebook" 
+          size={30} 
+          color="blue" 
+          onPress={() => {
+            Linking.openURL('https://www.facebook.com/')}}
+        />
+        <Ionicons 
+          name="logo-instagram" 
+          size={30} 
+          color="red" 
+          onPress={() => {
+            Linking.openURL('https://www.instagram.com/')}}
+        />
+        <Ionicons 
+          name="logo-twitter" 
+          size={30} 
+          color="blue" 
+          onPress={() => {
+            Linking.openURL('https://twitter.com/?lang=en')}}
+        />
+        <Ionicons
+           name="logo-whatsapp" 
+           size={30} 
+           color="green" 
+           onPress={() => {
+            Linking.openURL('https://www.whatsapp.com/')}}
+        />
+        <Ionicons
+           name="mail-outline" 
+           size={30} 
+           color="red" 
+           onPress={() => {
+            Linking.openURL('mailto:chaminduhansana5@gmail.com')}}
+        />
+      </View>
 
-      <Card>
-        <Card.Content>
-          <Paragraph>
-            Instagram{"     "}
-            <Ionicons name="logo-instagram" size={20} color="red" />
-          </Paragraph>
-        </Card.Content>
-      </Card>
-
-      <Card>
-        <Card.Content>
-          <Paragraph>
-            Twitter{"      "}
-            <Ionicons name="logo-twitter" size={20} color="blue" />
-          </Paragraph>
-        </Card.Content>
-      </Card>
-
-      <Card>
-        <Card.Content>
-          <Paragraph>
-            Whatsapp{"      "}
-            <Ionicons name="logo-whatsapp" size={20} color="green" />
-          </Paragraph>
-        </Card.Content>
-      </Card>
     </View>
   );
 };
